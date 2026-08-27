d7ec5ba851a458a6d0b98bbacd3e3986559e7680
Vijaykeerthi <keerthivijay89@gmail.com>
2026-08-27 11:09:58 +0530

    Added PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      // 👇 This forces the plugin to generate the manifest during local development
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: 'Shoping App',
+        short_name: 'ReactPWA',
+        description: 'My awesome Progressive Web App built with React and Vite',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'logo-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
